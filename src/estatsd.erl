7dcae3dd27053016ae5bd8599006c2f9a57bf808
Stephen Hawkridge <stephenh@miranetworks.net>
2019-05-06 09:29:20 +0200

    Modifications to allow tags.

--- src/estatsd.erl
+++ src/estatsd.erl
@@ -40,49 +40,48 @@
 
 -export([
          start/0,
-         gauge/2,
-         increment/1, increment/2, increment/3,
-         decrement/1, decrement/2, decrement/3,
-         timing/2
+         timing/2,timing/3,
+         gauge/2,gauge/3,
+         increment/1, increment/2, increment/3, increment/4,
+         decrement/1, decrement/2, decrement/3, decrement/4
         ]).
 
 -define(SERVER, estatsd_server).
 
-start() ->
-    application:start(estatsd).
+start() -> application:start(estatsd).
 
-timing(Key, Fun) when is_function(Fun,0) ->
+timing(Key, Fun) when is_function(Fun,0) -> timing(Key, "", Fun);
+timing(Key, StartTime = {_,_,_})         -> timing(Key, "", StartTime);
+timing(Key, Duration)                    -> timing(Key, "", Duration).
+
+timing(Key, Tags, Fun) when is_function(Fun,0) ->
     Start = os:timestamp(),
     Return = Fun(),
-    timing(Key, Start),
+    timing(Key, Tags, Start),
     Return;
 
 % Convenience: just give it the now() tuple when the work started
-timing(Key, StartTime = {_,_,_}) ->
+timing(Key, Tags, StartTime = {_,_,_}) ->
     Dur = erlang:round(timer:now_diff(os:timestamp(), StartTime)/1000),
-    timing(Key,Dur);
+    timing(Key, Tags,Dur);
 
 % Log timing information, ms
-timing(Key, Duration) when is_integer(Duration) -> 
-    gen_server:cast(?SERVER, {timing, Key, Duration});
-
-timing(Key, Duration) -> 
-    gen_server:cast(?SERVER, {timing, Key, erlang:round(Duration)}).
-
-
-
-
-% Increments one or more stats counters
-increment(Key) -> increment(Key, 1, 1).
-increment(Key, Amount) -> increment(Key, Amount, 1).
-increment(Key, Amount, Sample) ->
-    gen_server:cast(?SERVER, {increment, Key, Amount, Sample}).
-
-decrement(Key) -> decrement(Key, -1, 1).
-decrement(Key, Amount) -> decrement(Key, Amount, 1).
-decrement(Key, Amount, Sample) ->
-    increment(Key, 0 - Amount, Sample).
+timing(Key, Tags, Duration) when is_integer(Duration) -> gen_server:cast(?SERVER, {timing, Key, Tags, Duration});
+timing(Key, Tags, Duration)                           -> gen_server:cast(?SERVER, {timing, Key, Tags, erlang:round(Duration)}).
 
 % Sets a gauge value
-gauge(Key, Value) when is_number(Value) ->
-    gen_server:cast(?SERVER, {gauge, Key, Value}).
+gauge(Key, Value)                              -> gauge(Key, "", Value).
+gauge(Key, Tags, Value) when is_number(Value)  -> gen_server:cast(?SERVER, {gauge, Key, Tags, Value}).
+
+% Increments one or more stats counters
+increment(Key)                                 -> increment(Key, "").
+increment(Key, Tags)   when is_list(Tags)      -> increment(Key, Tags, 1, 1);
+increment(Key, Amount) when is_integer(Amount) -> increment(Key, "", Amount).
+increment(Key, Tags, Amount)                   -> increment(Key, Tags, Amount, 1).
+increment(Key, Tags, Amount, Sample)           -> gen_server:cast(?SERVER, {increment, Key, Tags, Amount, Sample}).
+
+decrement(Key)                                 -> decrement(Key, "").
+decrement(Key, Tags) when is_list(Tags)        -> decrement(Key, Tags, -1, 1);
+decrement(Key, Amount) when is_integer(Amount) -> decrement(Key, "", Amount).
+decrement(Key, Tags, Amount)                   -> decrement(Key, Tags, Amount, 1).
+decrement(Key, Tags, Amount, Sample)           -> increment(Key, Tags, 0 - Amount, Sample).
--- src/estatsd_server.erl
+++ src/estatsd_server.erl
@@ -28,11 +28,7 @@
                 tx_bytes            % number of bytes sent to graphite
                }).
 
-start_link(Options) ->
-    gen_server:start_link({local, ?MODULE}, 
-                          ?MODULE, 
-                          Options, 
-                          []).
+start_link(Options) -> gen_server:start_link({local, ?MODULE}, ?MODULE, Options, []).
 
 %%
 
@@ -43,8 +39,7 @@ init([FlushIntervalMs, GraphiteHost, GraphitePort, VmMetrics, PathPrefix]) ->
     ets:new(statsd, [named_table, set]),
     ets:new(statsdgauge, [named_table, set]),
     %% Flush out stats to graphite periodically
-    {ok, Tref} = timer:apply_interval(FlushIntervalMs, gen_server, cast, 
-                                                       [?MODULE, flush]),
+    {ok, Tref} = timer:apply_interval(FlushIntervalMs, gen_server, cast, [?MODULE, flush]),
     State = #state{ timers          = gb_trees:empty(),
                     flush_interval  = FlushIntervalMs,
                     flush_timer     = Tref,
@@ -56,33 +51,28 @@ init([FlushIntervalMs, GraphiteHost, GraphitePort, VmMetrics, PathPrefix]) ->
                   },
     {ok, State}.
 
-handle_cast({gauge, Key, Value0}, State) ->
+handle_cast({gauge, Key, Tags, Value0}, State) ->
     Value = {Value0, num2str(unixtime())},
-    case ets:lookup(statsdgauge, Key) of
-        [] ->
-            ets:insert(statsdgauge, {Key, [Value]});
-        [{Key, Values}] ->
-            ets:insert(statsdgauge, {Key, [Value | Values]})
+    case ets:lookup(statsdgauge, {Key, Tags}) of
+        []                     -> ets:insert(statsdgauge, {{Key, Tags}, [Value]});
+        [{{Key, Tags}, Values}] -> ets:insert(statsdgauge, {{Key, Tags}, [Value | Values]})
     end,
     {noreply, State};
 
-handle_cast({increment, Key, Delta0, Sample}, State) when Sample >= 0, Sample =< 1 ->
+handle_cast({increment, Key, Tags, Delta0, Sample}, State) when Sample >= 0, Sample =< 1 ->
     Delta = Delta0 * ( 1 / Sample ), %% account for sample rates < 1.0
-    case ets:lookup(statsd, Key) of
-        [] ->
-            ets:insert(statsd, {Key, {Delta,1}});
-        [{Key,{Tot,Times}}] ->
-            ets:insert(statsd, {Key,{Tot+Delta, Times+1}}),
+    case ets:lookup(statsd, {Key, Tags}) of
+        []                          -> ets:insert(statsd, {{Key, Tags}, {Delta,1}});
+        [{{Key, Tags},{Tot,Times}}] -> 
+            ets:insert(statsd, {{Key, Tags}, {Tot+Delta, Times+1}}),
             ok
     end,
     {noreply, State};
 
-handle_cast({timing, Key, Duration}, State) ->
-    case gb_trees:lookup(Key, State#state.timers) of
-        none ->
-            {noreply, State#state{timers = gb_trees:insert(Key, [Duration], State#state.timers)}};
-        {value, Val} ->
-            {noreply, State#state{timers = gb_trees:update(Key, [Duration|Val], State#state.timers)}}
+handle_cast({timing, Key, Tags, Duration}, State) ->
+    case gb_trees:lookup({Key, Tags}, State#state.timers) of
+        none         -> {noreply, State#state{timers = gb_trees:insert({Key, Tags}, [Duration], State#state.timers)}};
+        {value, Val} -> {noreply, State#state{timers = gb_trees:update({Key, Tags}, [Duration|Val], State#state.timers)}}
     end;
 
 handle_cast({tx_bytes, Bytes}, State) ->
@@ -110,15 +100,16 @@ terminate(_, _)             -> ok.
 %% INTERNAL STUFF
 
 send_to_graphite(Msg, State) ->
-    %error_logger:info_msg("estatsd tx ~s\n", [Msg]),
+    Message = lists:flatten(Msg),
+    error_logger:info_msg("estatsd tx ~s\n", [Msg]),
     case gen_tcp:connect(State#state.graphite_host,
                          State#state.graphite_port,
                          [list, {packet, 0}], 60000) of
         {ok, Sock} ->
             inet:setopts(Sock,[{send_timeout, 120000}]),
-            gen_tcp:send(Sock, Msg),
+            gen_tcp:send(Sock, Message),
             gen_tcp:close(Sock),
-            gen_server:cast(?MODULE, {tx_bytes, iolist_size(Msg)}),
+            gen_server:cast(?MODULE, {tx_bytes, iolist_size(Message)}),
             ok;
         E ->
             error_logger:error_msg("Failed to connect to graphite: ~p", [E]),
@@ -126,11 +117,9 @@ send_to_graphite(Msg, State) ->
     end.
 
 % this string munging is damn ugly compared to javascript :(
-key2str(K) when is_atom(K) -> 
-    atom_to_list(K);
-key2str(K) when is_binary(K) -> 
-    key2str(binary_to_list(K));
-key2str(K) when is_list(K) ->
+key2str(K) when is_atom(K)   -> atom_to_list(K);
+key2str(K) when is_binary(K) -> key2str(binary_to_list(K));
+key2str(K) when is_list(K)   ->
     {ok, R1} = re:compile("\\s+"),
     {ok, R2} = re:compile("/"),
     {ok, R3} = re:compile("[^a-zA-Z_\\-0-9\\.;=]"),
@@ -148,47 +137,44 @@ unixtime()  -> {Meg,S,_Mic} = os:timestamp(), Meg*1000000 + S.
 do_report(All, Gauges, State) ->
     % One time stamp string used in all stats lines:
     TsStr = num2str(unixtime()),
-    {MsgCounters, NumCounters}         = do_report_counters(All, TsStr, State),
-    {MsgTimers,   NumTimers}           = do_report_timers(TsStr, State),
-    {MsgGauges,   NumGauges}           = do_report_gauges(Gauges, State),
-    {MsgVmMetrics,   NumVmMetrics}  = do_report_vm_metrics(TsStr, State),
+    {MsgCounters,  NumCounters}   = do_report_counters(All, TsStr, State),
+    {MsgTimers,    NumTimers}     = do_report_timers(TsStr, State),
+    {MsgGauges,    NumGauges}     = do_report_gauges(Gauges, State),
+    {MsgVmMetrics, NumVmMetrics}  = do_report_vm_metrics(TsStr, State),
     %% REPORT TO GRAPHITE
-    case NumTimers + NumCounters + NumGauges + NumVmMetrics of
+    case NumCounters + NumTimers + NumGauges + NumVmMetrics of
         0 -> nothing_to_report;
         NumStats ->
             MsgEstatsd = lists:map(fun({Key, Val}) ->
-                        [
-                            State#state.path_prefix, ".estatsd.", key2str(Key), " ",
-                            io_lib:format("~w", [Val]), " ",
-                            TsStr, "\n"
-                        ]
-                end, [
-                    {num_stats, NumStats},
-                    {tx_bytes, State#state.tx_bytes}
-                ]),
-            FinalMsg = [ MsgCounters,
-                         MsgTimers,
-                         MsgGauges,
-                         MsgVmMetrics,
-                         %% Also graph some estatsd stats!
-                         MsgEstatsd
-                       ],
+                       [
+                           State#state.path_prefix, ".estatsd.", key2str(Key), " ",
+                           io_lib:format("~w", [Val]), " ",
+                           TsStr, "\n"
+                       ]
+            end,
+            [{num_stats, NumStats}, {tx_bytes, State#state.tx_bytes}]),
+            
+            FinalMsg = [MsgCounters
+                       ,MsgTimers
+                       ,MsgGauges
+                       ,MsgVmMetrics
+                       ,MsgEstatsd],
             send_to_graphite(FinalMsg, State)
     end.
 
 do_report_counters(All, TsStr, State) ->
     Msg = lists:foldl(
-                fun({Key, {Val0,NumVals}}, Acc) ->
+                fun({{Key, Tags}, {Val0,_}}, Acc) ->
                         KeyS = key2str(Key),
                         Val = Val0 / (State#state.flush_interval/1000),
                         %% Build stats string for graphite
-                        Fragment = [ State#state.path_prefix, ".counters.", KeyS, " ", 
+                        Fragment = [ State#state.path_prefix, ".", KeyS, Tags, " ", 
                                      io_lib:format("~w", [Val]), " ", 
-                                     TsStr, "\n",
-
-                                     State#state.path_prefix, ".counters.counts.", KeyS, " ", 
-                                     io_lib:format("~w",[NumVals]), " ", 
                                      TsStr, "\n"
+
+                                     % State#state.path_prefix, ".counters.counts.", KeyS, " ", 
+                                     % io_lib:format("~w",[NumVals]), " ", 
+                                     % TsStr, "\n"
                                    ],
                         [ Fragment | Acc ]                    
                 end, [], All),
@@ -197,7 +183,7 @@ do_report_counters(All, TsStr, State) ->
 do_report_timers(TsStr, State) ->
     Timings = gb_trees:to_list(State#state.timers),
     Msg = lists:foldl(
-        fun({Key, Vals}, Acc) ->
+        fun({{Key, Tags}, Vals}, Acc) ->
                 KeyS = key2str(Key),
                 Values          = lists:sort(Vals),
                 Count           = length(Values),
@@ -210,14 +196,14 @@ do_report_timers(TsStr, State) ->
                 MaxAtThreshold  = lists:nth(NumInThreshold, Values),
                 Mean            = lists:sum(Values1) / NumInThreshold,
                 %% Build stats string for graphite
-                Startl          = [ State#state.path_prefix, ".timers.", KeyS, "." ],
+                Startl          = [ State#state.path_prefix, ".", KeyS],
                 Endl            = [" ", TsStr, "\n"],
                 Fragment        = [ [Startl, Name, " ", num2str(Val), Endl] || {Name,Val} <-
-                                  [ {"mean", Mean},
-                                    {"upper", Max},
-                                    {"upper_"++num2str(PctThreshold), MaxAtThreshold},
-                                    {"lower", Min},
-                                    {"count", Count}
+                                  [ {Tags ++ ";agg=mean", Mean},
+                                    {Tags ++ ";agg=upper", Max},
+                                    {Tags ++ ";agg=upper_"++num2str(PctThreshold), MaxAtThreshold},
+                                    {Tags ++ ";agg=lower", Min},
+                                    {Tags ++ ";agg=count", Count}
                                   ]],
                 [ Fragment | Acc ]
         end, [], Timings),
@@ -225,13 +211,13 @@ do_report_timers(TsStr, State) ->
 
 do_report_gauges(Gauges, State) ->
     Msg = lists:foldl(
-        fun({Key, Vals}, Acc) ->
+        fun({{Key, Tags}, Vals}, Acc) ->
             KeyS = key2str(Key),
             Fragments = lists:foldl(
                 fun ({Val, TsStr}, KeyAcc) ->
                     %% Build stats string for graphite
                     Fragment = [
-                        State#state.path_prefix, ".gauges.", KeyS, " ",
+                        State#state.path_prefix, ".", KeyS, Tags, " ",
                         io_lib:format("~w", [Val]), " ",
                         TsStr, "\n"
                     ],
